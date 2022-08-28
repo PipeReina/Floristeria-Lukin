ba1c5e47ac2ac366b354011bf67b14ff7a90fb5c
valentina1901 <vgaviria415@misena.edu.co>
2022-08-28 02:38:23 -0500

    Empleando tabla comision y horas extra

---
+++ src/main/java/com/nomina/nomina1/controller/Comisioncontroller.java
@@ -0,0 +1,93 @@
+package com.nomina.nomina1.controller;
+
+import javax.validation.Valid;
+import com.nomina.nomina1.model.comision;
+import com.nomina.nomina1.services.Icomisionservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+@Controller
+@SessionAttributes("comision")
+@RequestMapping("/comision")
+public class Comisioncontroller {
+    
+    @Autowired
+    private Icomisionservice com;
+
+    @GetMapping(path={"/listarcom","","/"})
+    public String listarcom(Model mo){
+        mo.addAttribute("comisiones", com.findAll());
+        return "comision/listarcom";
+    }
+ 
+    @GetMapping("/vercom")
+    public String vercom(Model mo){
+        comision comi = new comision();
+        comi.setIdComision(1); 
+        mo.addAttribute("comision",comi);
+        return "comision/vercom";
+    }
+
+    @GetMapping("/buscarcom/{idComision}")
+    public String buscarcom(@PathVariable Integer idComision,Model mo){
+        comision comision = null;
+        if(idComision>0){
+            comision = com.findOne(idComision);
+        }else{
+            return "redirect:listarcom";
+        }
+        mo.addAttribute("comision",comision);
+        mo.addAttribute("accion");
+        return "comision/formcom";  
+    }  
+
+    @GetMapping("/formcom")
+    public String formcom(Model mo){
+        comision comision = new comision();
+        mo.addAttribute("comision", comision);
+        mo.addAttribute("accion");
+        return "comision/formcom";
+    }
+
+    @PostMapping("add")
+    public String add(@Valid comision comision, BindingResult res, Model mo, SessionStatus status){
+        if(res.hasErrors()){
+            return "comision/formcom";
+        }
+        com.save(comision);
+        status.setComplete();
+        return "redirect:listarcom";
+    }
+
+    @GetMapping("/estado/{idComision}")
+    public String estado(@PathVariable Integer idComision,Model mo){
+        comision comision=null;
+        comision=com.findOne(idComision);
+        if(comision.getEstadoCom()==true){
+           comision.setEstadoCom(false);
+       }else if (comision.getEstadoCom()==false){
+           comision.setEstadoCom(true);
+       }
+       com.save(comision);
+       return "redirect:../";
+    }
+
+
+
+    @GetMapping("/delete/{idComision}")
+    public String delete (@PathVariable Integer idComision){
+        if(idComision > 0){
+            com.delete(idComision);
+        }
+        return "redirect:../listarcom";
+    }
+}
---
+++ src/main/java/com/nomina/nomina1/model/comision.java
@@ -0,0 +1,97 @@
+package com.nomina.nomina1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name="comisiones")
+public class comision {
+    
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer IdComision;
+
+    @Column (length = 50)
+    private Integer CostoVenta;
+
+    @Column (length = 50, nullable=true)
+    private String FechaCom;
+    
+    @Column (length = 50, nullable=true)
+    private Boolean EstadoCom;
+
+    /*@ManyToOne (fetch = FetchType.LAZY)
+    private Empleado IdEmpleadoFK;*/
+
+
+    //constructores
+    public comision(){
+
+    }
+
+
+    public comision(Integer idComision, Integer costoVenta, String fechaCom, Boolean estadoCom/*, Empleado idEmpleadoFK*/) {
+        IdComision = idComision;
+        CostoVenta = costoVenta;
+        FechaCom = fechaCom;
+        EstadoCom = estadoCom;
+       /*IdEmpleadoFK = idEmpleadoFK;*/
+    }
+
+
+    public Integer getIdComision() {
+        return IdComision;
+    }
+
+
+    public void setIdComision(Integer idComision) {
+        IdComision = idComision;
+    }
+
+
+    public Integer getCostoVenta() {
+        return CostoVenta;
+    }
+
+
+    public void setCostoVenta(Integer costoVenta) {
+        CostoVenta = costoVenta;
+    }
+
+
+    public String getFechaCom() {
+        return FechaCom;
+    }
+
+
+    public void setFechaCom(String fechaCom) {
+        FechaCom = fechaCom;
+    }
+
+
+    public Boolean getEstadoCom() {
+        return EstadoCom;
+    }
+
+    public void setEstadoCom(Boolean estadoCom) {
+        EstadoCom = estadoCom;
+    }
+
+
+    /*public Empleado getIdEmpleadoFK() {
+        return IdEmpleadoFK;
+    }
+
+
+    public void setIdEmpleadoFK(Empleado idEmpleadoFK) {
+        IdEmpleadoFK = idEmpleadoFK;
+    }*/
+
+     }
+
+    
